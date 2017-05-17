fa44fd91875b47f4e9baef933e1a4130da6159a0
John <potatoking.john@gmail.com>
2017-05-17 10:16:43 -0600

    update files

--- ElementID_GUI_4.m
+++ ElementID_GUI_4.m
@@ -1,35 +1,35 @@
-function varargout = ElementID_GUI_3(varargin)
-% ELEMENTID_GUI_3 MATLAB code for ElementID_GUI_3.fig
-%      ELEMENTID_GUI_3, by itself, creates a new ELEMENTID_GUI_3 or raises the existing
+function varargout = ElementID_GUI_4(varargin)
+% ELEMENTID_GUI_4 MATLAB code for ElementID_GUI_4.fig
+%      ELEMENTID_GUI_4, by itself, creates a new ELEMENTID_GUI_4 or raises the existing
 %      singleton*.
 %
-%      H = ELEMENTID_GUI_3 returns the handle to a new ELEMENTID_GUI_3 or the handle to
+%      H = ELEMENTID_GUI_4 returns the handle to a new ELEMENTID_GUI_4 or the handle to
 %      the existing singleton*.
 %
-%      ELEMENTID_GUI_3('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in ELEMENTID_GUI_3.M with the given input arguments.
+%      ELEMENTID_GUI_4('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in ELEMENTID_GUI_4.M with the given input arguments.
 %
-%      ELEMENTID_GUI_3('Property','Value',...) creates a new ELEMENTID_GUI_3 or raises the
+%      ELEMENTID_GUI_4('Property','Value',...) creates a new ELEMENTID_GUI_4 or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before ElementID_GUI_3_OpeningFcn gets called.  An
+%      applied to the GUI before ElementID_GUI_4_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to ElementID_GUI_3_OpeningFcn via varargin.
+%      stop.  All inputs are passed to ElementID_GUI_4_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help ElementID_GUI_3
+% Edit the above text to modify the response to help ElementID_GUI_4
 
-% Last Modified by GUIDE v2.5 11-Dec-2016 20:30:18
+% Last Modified by GUIDE v2.5 12-Dec-2016 15:52:00
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
     'gui_Singleton',  gui_Singleton, ...
-    'gui_OpeningFcn', @ElementID_GUI_3_OpeningFcn, ...
-    'gui_OutputFcn',  @ElementID_GUI_3_OutputFcn, ...
+    'gui_OpeningFcn', @ElementID_GUI_4_OpeningFcn, ...
+    'gui_OutputFcn',  @ElementID_GUI_4_OutputFcn, ...
     'gui_LayoutFcn',  [] , ...
     'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -49,15 +49,15 @@ end
 % 2. Make Neighbor selection function separate from Next File function
 
 
-% --- Executes just before ElementID_GUI_3 is made visible.
-function ElementID_GUI_3_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before ElementID_GUI_4 is made visible.
+function ElementID_GUI_4_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to ElementID_GUI_3 (see VARARGIN)
+% varargin   command line arguments to ElementID_GUI_4 (see VARARGIN)
 
-% Choose default command line output for ElementID_GUI_3
+% Choose default command line output for ElementID_GUI_4
 handles.output = hObject;
 
 handles.countLoc = 1;
@@ -79,12 +79,12 @@ set(handles.act_Opts,'Enable','off')
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes ElementID_GUI_3 wait for user response (see UIRESUME)
+% UIWAIT makes ElementID_GUI_4 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = ElementID_GUI_3_OutputFcn(hObject, eventdata, handles)
+function varargout = ElementID_GUI_4_OutputFcn(hObject, eventdata, handles)
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
